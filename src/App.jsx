1b5688bd027a6eab547b80c6d7c9a7c0dfc727ad
Jitendra <jitendraphone7505@gmail.com>
2026-04-05 22:50:57 +0530

    Handling routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
-import Hero from "./components/Hero";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import About from "./components/about/About";
+import Projects from "./components/projects/Projects";
+import Contact from "./components/Contactus/Contact";
 import Nav from "./components/Nav";
 
 function App() {
@@ -6,7 +10,12 @@ function App() {
     <>
       <div className="main h-[100vh] w-[100vw] bg-gradient-to-b from-[#d1d0d0] via-[#87C6C1] to-[#3C3434]  flex flex-col items-center  gap-7">
         <Nav />
-        <Hero />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact-us" element={<Contact />} />
+        </Routes>
       </div>
     </>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 function Nav() {
   return (
     <div className="navbar h-[10vh] w-[100vw]  bg-transparent flex items-center justify-between text-2xl text-zinc-800 p-10 ">
@@ -9,30 +10,35 @@ function Nav() {
         <img src="./public/logo.png" alt="" className="h-[12vh] w-[13vw]" />
       </div>
       <div className="navElement flex gap-20 mr-10 ">
-        <h1
+        <Link
+          to="/"
           className=" border-b-2 border-transparent   hover:border-b-2 
           hover:border-amber-50 hover:text-shadow-sm hover:text-shadow-[#828281]"
         >
           HOME
-        </h1>
-        <h1
-          className="border-b-2 border-transparent   
+        </Link>
+
+        <Link
+          to="/about"
+          className=" border-b-2 border-transparent   hover:border-b-2 
           hover:border-amber-50 hover:text-shadow-sm hover:text-shadow-[#828281]"
         >
-          ABOUT
-        </h1>
-        <h1
-          className="border-b-2 border-transparent   
+          About
+        </Link>
+        <Link
+          to="/projects"
+          className=" border-b-2 border-transparent   hover:border-b-2 
           hover:border-amber-50 hover:text-shadow-sm hover:text-shadow-[#828281]"
         >
-          PROJECTS
-        </h1>
-        <h1
-          className="border-b-2 border-transparent   
+          Projects
+        </Link>
+        <Link
+          to="/contact-us"
+          className=" border-b-2 border-transparent   hover:border-b-2 
           hover:border-amber-50 hover:text-shadow-sm hover:text-shadow-[#828281]"
         >
-          CONTACT US
-        </h1>
+          Contact Us
+        </Link>
       </div>
     </div>
   );
