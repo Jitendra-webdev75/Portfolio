28375a2f9a412c151b20eb7ae59f6c2d0c1357c0
Jitendra <jitendraphone7505@gmail.com>
2026-04-13 23:52:44 +0530

    making contact page

--- src/components/Contactus/Contact.jsx
+++ src/components/Contactus/Contact.jsx
@@ -2,21 +2,26 @@ import Galaxy from "../Galaxy";
 
 function Contact() {
   return (
-    <div className="h-[100vh] w-[100vw] bg-zinc-950">
-      <Galaxy
-        mouseRepulsion
-        mouseInteraction
-        density={1}
-        glowIntensity={0.2}
-        saturation={0}
-        hueShift={140}
-        twinkleIntensity={0.3}
-        rotationSpeed={0.1}
-        repulsionStrength={2}
-        autoCenterRepulsion={0}
-        starSpeed={0.5}
-        speed={1}
-      />
+    <div className="h-screen w-screen bg-transparent p-10 ">
+      <div className="absolute inset-0 z-0">
+        <Galaxy
+          mouseRepulsion
+          mouseInteraction
+          density={1}
+          glowIntensity={0.2}
+          saturation={0}
+          hueShift={140}
+          twinkleIntensity={0.3}
+          rotationSpeed={0.1}
+          repulsionStrength={2}
+          autoCenterRepulsion={0}
+          starSpeed={0.5}
+          speed={1}
+        />
+      </div>
+      <div className="content h-full w-full z-20  flex flex-col items-center">
+        <h1 className="text-6xl text-amber-50  ">Get In Touch</h1>
+      </div>
     </div>
   );
 }
